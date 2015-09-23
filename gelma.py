66983b5e78d90252c0fa5b8d9321b7db92fcfdd2
endeios@gmail.com <endeios@gmail.com>
2015-09-23 17:56:43 +0100

    caricamento da csv

--- gelma.py
+++ gelma.py
@@ -12,22 +12,30 @@ class GelmaGui(QtGui.QMainWindow):
         QtGui.QMainWindow.__init__(self,parent=parent)
         uic.loadUi('gui.ui',self)
         self.logger = logging.getLogger('GelmaGui')
-        self.my_model = GelmaModel()
+        self.my_model = GelmaModel('lol.csv')
         self.tableView.setModel(self.my_model)
+
+    def load(self):
+
+        pass
+
+    def save(self):
+        pass
+
 
 
 class GelmaModel(QtCore.QAbstractTableModel):
     
-    def __init__(self,parent=None,*args):
+    def __init__(self,filename,parent=None,*args):
         super(GelmaModel,self).__init__(parent,*args)
-        self.header = ['id','link','value']
-        self.data_model = [
-            [1,2,3],
-            [4,6,7],
-            [5,6,8],
-            [9,1,4]
-        ]
-    
+        with open(filename) as data_file:
+            self.reader = csv.reader(data_file)
+            self.data_model = list()
+            for i in self.reader:
+                self.data_model.append(i)
+
+            self.header = self.data_model[0]
+
     def data(self,index,role):
         if not index.isValid():
             return QVariant()
@@ -35,12 +43,12 @@ class GelmaModel(QtCore.QAbstractTableModel):
             return QVariant()
         if self.data_model != None:
             keys = self.header
-            info = self.data_model[index.row()][index.column()]
+            info = self.data_model[index.row()+1][index.column()]
             return QVariant(info)
         return QVariant()
 
     def rowCount(self,parent):
-        return len(self.data_model)
+        return len(self.data_model)-1
 
     def columnCount(self,parent):
 #        self.logger.debug("Model is %s"%self.model)
